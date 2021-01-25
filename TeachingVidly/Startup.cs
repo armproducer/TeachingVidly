2aedbe1737d1d807ff9e185cffec76013ff412fe
Armen <armproducer@gmail.com>
2021-01-25 12:01:45 +0300

    Add project files.

---
+++ TeachingVidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeachingVidly.Startup))]
+namespace TeachingVidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
